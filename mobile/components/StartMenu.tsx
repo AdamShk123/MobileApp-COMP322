f77b2fd48928bfdd2ce4452843bf41d15d9f1ca5
AdamShk123 <adamshkolnik5@gmail.com>
2023-09-07 10:43:54 -0500

    added global stylesheet and modified StartMenu styling

--- mobile/components/StartMenu.tsx
+++ mobile/components/StartMenu.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import {Text, Button, View} from 'react-native'
-
+import {StyleSheet, Pressable, View, Text} from 'react-native'
+import colors from '../styles'
 import {NativeStackNavigationProp} from '@react-navigation/native-stack'
 import { RootStackParamList } from '../App';
 
@@ -10,11 +10,42 @@ type Props = {
 
 const StartMenu = ({navigation}: Props) => {
     return (
-        <View>
-            <Button title='Log In'/>
-            <Button title='Create Account' onPress={() => navigation.navigate('CreateAccount')}/>
+        <View style={{flex:1, alignSelf:'center'}}>
+            <Text style={styles.title}>D&D Universe</Text>
+            <View style={styles.view}>
+                <Pressable style={[styles.button,colors.backgroundBlack]}>
+                    <Text style={[colors.textWhite, styles.text]}>Log In</Text>
+                </Pressable>
+                <Pressable style={[styles.button,colors.backgroundBlack]} onPress={() => navigation.navigate('CreateAccount')}>
+                    <Text style={[colors.textWhite, styles.text]}>Create Account</Text>
+                </Pressable>
+            </View>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    button: {
+        borderRadius: 3,
+        marginBottom: 5,
+    },
+    view: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+    },
+    text: {
+        padding: 3,
+        fontSize: 15,
+    },
+    title: {
+        color: 'red',
+        fontSize: 24,
+        fontWeight: 'bold',
+        textShadowRadius: 5,
+    }
+});
+
 
 export default StartMenu;
---
+++ mobile/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const colors = StyleSheet.create({
+    backgroundBlack: {
+        backgroundColor: 'black',
+    },
+    backgroundWhite: {
+        backgroundColor: 'white',
+    },
+    textBlack: {
+        color: 'black',
+    },
+    textWhite: {
+        color: 'white',
+    }
+});
+
+export default colors;
+
